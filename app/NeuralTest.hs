2bad112440ce868d564d30c50d17a56412602f4d
Justin Le <justin@jle.im>
2017-03-02 15:57:50 -0800

    renamed tensortest to neuraltest, with some documentation

--- app/NeuralTest.hs
+++ app/NeuralTest.hs
@@ -15,9 +15,11 @@
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns         #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 import           Data.Functor
 import           Data.Kind
+import           Data.Maybe
 import           Data.Singletons
 import           Data.Singletons.Prelude
 import           Data.Singletons.TypeLits
@@ -29,53 +31,42 @@ import           Numeric.Backprop.Iso
 import           Numeric.Backprop.Op
 import           Numeric.LinearAlgebra.Static hiding (dot)
 import           System.Random.MWC
-import           Type.Class.Witness hiding           (outer)
-import           Type.Family.List
 import qualified Generics.SOP                        as SOP
 
-data Tensor :: [Nat] -> Type where
-    TS :: { unTS :: Double } -> Tensor '[]
-    TV :: { unTV :: R n    } -> Tensor '[n]
-    TM :: { unTM :: L m n  } -> Tensor '[m,n]
-
-deriving instance ListC (KnownNat <$> ns) => Show (Tensor ns)
+-- Basic matrix/vector operations, as Ops
 
 matVec
     :: (KnownNat m, KnownNat n)
-    => Op '[ Tensor '[m,n] , Tensor '[n] ] (Tensor '[m])
-matVec = op2' $ \case
-    TM m -> \case
-      TV v -> ( TV (m #> v)
-              , \(maybe 1 unTV -> g) ->
-                   (TM (g `outer` v), TV (tr m #> g))
-              )
+    => Op '[ L m n, R n ] (R m)
+matVec = op2' $ \m v -> ( m #> v
+                        , \(fromMaybe 1 -> g) ->
+                             (g `outer` v, tr m #> g)
+                        )
 
 dot :: KnownNat n
-    => Op '[ Tensor '[n] , Tensor '[n] ] (Tensor '[])
-dot = op2' $ \case
-    TV x -> \case
-      TV y -> ( TS (x <.> y)
-              , \case Nothing      ->
-                        (TV y            , TV x            )
-                      Just (TS g)  ->
-                        (TV (konst g * y), TV (x * konst g))
-              )
+    => Op '[ R n, R n ] Double
+dot = op2' $ \x y -> ( x <.> y
+                     , \case Nothing -> (y, x)
+                             Just g  -> (konst g * y, x * konst g)
+                     )
 
-logistic :: Floating a => a -> a
-logistic x = 1 / (1 + exp (-x))
+-- Containers holding our network parameters
 
 data Layer :: Nat -> Nat -> Type where
-    Layer :: { _lWeights :: Tensor '[m, n]
-             , _lBiases  :: Tensor '[m]
+    Layer :: { _lWeights :: L m n
+             , _lBiases  :: R m
              }
           -> Layer n m
       deriving (Show, Generic)
-instance SOP.Generic (Layer n m)
 
 data Network :: Nat -> [Nat] -> Nat -> Type where
     NØ   :: !(Layer a b) -> Network a '[] b
     (:&) :: !(Layer a b) -> Network b bs c -> Network a (b ': bs) c
 
+-- Generic instance so we can use gTuple iso
+instance SOP.Generic (Layer n m)
+
+-- Iso's for the Network constructors
 netExternal :: Iso' (Network a '[] b) (Tuple '[Layer a b])
 netExternal = iso (\case NØ x     -> x ::< Ø)
                   (\case I x :< Ø -> NØ x   )
@@ -84,15 +75,16 @@ netInternal :: Iso' (Network a (b ': bs) c) (Tuple '[Layer a b, Network b bs c])
 netInternal = iso (\case x :& xs          -> x ::< xs ::< Ø)
                   (\case I x :< I xs :< Ø -> x :& xs       )
 
+-- "Running" a network on a given input
 netOp
     :: forall s a bs c. (KnownNat a, KnownNat c)
     => Sing bs
-    -> BPOp s '[ Tensor '[a], Network a bs c ] (Tensor '[c])
+    -> BPOp s '[ R a, Network a bs c ] (R c)
 netOp sbs = go sbs
   where
     go :: forall d es. KnownNat d
         => Sing es
-        -> BPOp s '[ Tensor '[d], Network d es c ] (Tensor '[c])
+        -> BPOp s '[ R d, Network d es c ] (R c)
     go = \case
       SNil -> withInps $ \(x :< n :< Ø) -> do
         l :< Ø       <- netExternal #<~ n
@@ -103,28 +95,34 @@ netOp sbs = go sbs
         go ses       ~$ z :< n' :< Ø
     layerOp
         :: forall d e. (KnownNat d, KnownNat e)
-        => BPOp s '[ Tensor '[d], Layer d e ] (Tensor '[e])
+        => BPOp s '[ R d, Layer d e ] (R e)
     layerOp = withInps $ \(x :< l :< Ø) -> do
         w :< b :< Ø <- gTuple #<~ l
         y           <- opRef2 w x matVec
         y'          <- opRef2 y b (op2 (+))
         opRef1 y' (op1 logistic)
+      where
+        logistic x = 1 / (1 + exp (-x))
 
+
+-- calculate error based on target
 err
     :: KnownNat m
-    => Tensor '[m]
-    -> BPRef s rs (Tensor '[m])
-    -> BPOp s rs (Tensor '[])
+    => R m
+    -> BPRef s rs (R m)
+    -> BPOp s rs Double
 err targ r = do
-    let t = constRef targ
     d <- opRef2 r t $ op2 (-)
     opRef2 d d      $ dot
+  where
+    t = constRef targ
 
+-- gradient descent step
 train
     :: (KnownNat a, SingI bs, KnownNat c)
     => Double
-    -> Tensor '[a]
-    -> Tensor '[c]
+    -> R a
+    -> R c
     -> Network a bs c
     -> Network a bs c
 train r x t n = case backprop (err t =<< netOp sing) (x ::< n ::< Ø) of
@@ -147,101 +145,19 @@ main = withSystemRandom $ \g -> do
 
 
 
--- non-bp-related boilerplate for working with data types
+-- non-bp-related boilerplate and instances for working with data types
 
-instance SingI ns => Variate (Tensor ns) where
-    uniform g = case sing :: Sing ns of
-      SNil                             ->
-          TS <$> uniform g
-      SNat `SCons` SNil                ->
-          TV <$> (randomVector <$> uniform g <*> pure Uniform)
-      SNat `SCons` (SNat `SCons` SNil) ->
-          TM <$> (uniformSample <$> uniform g <*> pure 0 <*> pure 1)
-      _`SCons`(_`SCons`(_`SCons`_))    ->
-          error "unimplemented"
+instance KnownNat n => Variate (R n) where
+    uniform g = randomVector <$> uniform g <*> pure Uniform
+    uniformR (l, h) g = (\x -> x * (h - l) + l) <$> uniform g
 
-
-liftT0
-    :: SingI ns
-    => (forall a. Floating a => a)
-    -> Tensor ns
-liftT0 f = go sing
-  where
-    go :: forall ms. Sing ms -> Tensor ms
-    go = \case
-      SNil -> TS f
-      SNat `SCons` SNil -> TV f
-      SNat `SCons` (SNat `SCons` SNil) -> TM f
-      _ `SCons` (_ `SCons` (_ `SCons` _)) -> error "not implemented"
-
-liftT1
-    :: SingI ns
-    => (forall a. Floating a => a -> a)
-    -> Tensor ns
-    -> Tensor ns
-liftT1 f = go sing
-  where
-    go :: forall ms. Sing ms -> Tensor ms -> Tensor ms
-    go = \case
-      SNil                             -> \case TS x -> TS (f x)
-      SNat `SCons` SNil                -> \case TV x -> TV (f x)
-      SNat `SCons` (SNat `SCons` SNil) -> \case TM x -> TM (f x)
-      _ `SCons` (_ `SCons` (_ `SCons` _)) -> \case
-
-liftT2
-    :: SingI ns
-    => (forall a. Floating a => a -> a -> a)
-    -> Tensor ns
-    -> Tensor ns
-    -> Tensor ns
-liftT2 f = go sing
-  where
-    go :: forall ms. Sing ms -> Tensor ms -> Tensor ms -> Tensor ms
-    go = \case
-      SNil -> \case
-        TS x -> \case TS y -> TS (f x y)
-      SNat `SCons` SNil -> \case
-        TV x -> \case TV y -> TV (f x y)
-      SNat `SCons` (SNat `SCons` SNil) -> \case
-        TM x -> \case TM y -> TM (f x y)
-      _ `SCons` (_ `SCons` (_ `SCons` _)) -> \case
-
-instance SingI ns => Num (Tensor ns) where
-    (+) = liftT2 (+)
-    (-) = liftT2 (-)
-    (*) = liftT2 (*)
-    negate = liftT1 negate
-    signum = liftT1 signum
-    abs    = liftT1 abs
-    fromInteger x = liftT0 (fromInteger x)
-
-instance SingI ns => Fractional (Tensor ns) where
-    (/) = liftT2 (/)
-    recip = liftT1 recip
-    fromRational x = liftT0 (fromRational x)
-
-instance SingI ns => Floating (Tensor ns) where
-    pi = liftT0 pi
-    exp = liftT1 exp
-    log = liftT1 log
-    sqrt = liftT1 sqrt
-    (**) = liftT2 (**)
-    logBase = liftT2 logBase
-    sin = liftT1 sin
-    cos = liftT1 cos
-    tan = liftT1 tan
-    asin = liftT1 asin
-    acos = liftT1 acos
-    atan = liftT1 atan
-    sinh = liftT1 sinh
-    cosh = liftT1 cosh
-    tanh = liftT1 tanh
-    asinh = liftT1 asinh
-    acosh = liftT1 acosh
-    atanh = liftT1 atanh
+instance (KnownNat m, KnownNat n) => Variate (L m n) where
+    uniform g = uniformSample <$> uniform g <*> pure 0 <*> pure 1
+    uniformR (l, h) g = (\x -> x * (h - l) + l) <$> uniform g
 
 instance (KnownNat n, KnownNat m) => Variate (Layer n m) where
     uniform g = subtract 1 . (* 2) <$> (Layer <$> uniform g <*> uniform g)
+    uniformR (l, h) g = (\x -> x * (h - l) + l) <$> uniform g
 
 instance (KnownNat m, KnownNat n) => Num (Layer n m) where
     Layer w1 b1 + Layer w2 b2 = Layer (w1 + w2) (b1 + b2)
@@ -259,6 +175,7 @@ instance (KnownNat m, KnownNat n) => Fractional (Layer n m) where
 
 instance (KnownNat a, SingI bs, KnownNat c) => Variate (Network a bs c) where
     uniform g = genNet sing (uniform g)
+    uniformR (l, h) g = (\x -> x * (h - l) + l) <$> uniform g
 
 genNet
     :: forall f a bs c. (Applicative f, KnownNat a, KnownNat c)
