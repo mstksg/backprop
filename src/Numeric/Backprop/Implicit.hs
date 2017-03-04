8112b404de50a04121e7dd5a6b9d8c167520c859
Justin Le <justin@jle.im>
2017-03-04 14:33:18 -0800

    implicit backprop, wrapping over explicit backprop

---
+++ src/Numeric/Backprop/Implicit.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes       #-}
+
+module Numeric.Backprop.Implicit (
+  ) where
+
+import           Data.Type.Index
+import           Data.Type.Length
+import           Data.Type.Product
+import           Data.Type.Util
+import           Type.Class.Known
+import qualified Numeric.Backprop.Explicit        as BP
+import           Numeric.Backprop.Internal
+
+backprop'
+    :: Prod Summer rs
+    -> Prod Unity rs
+    -> (forall s. Prod (BPRef s rs) rs -> BPRef s rs a)
+    -> Tuple rs
+    -> (a, Tuple rs)
+backprop' ss us f = BP.backprop' ss us (return $ f (BP.inpRefs' (prodLength ss)))
+
+backprop
+    :: (Known Length rs, Every Num rs)
+    => (forall s. Prod (BPRef s rs) rs -> BPRef s rs a)
+    -> Tuple rs
+    -> (a, Tuple rs)
+backprop f = BP.backprop (return $ f BP.inpRefs)
+
+grad'
+    :: Prod Summer rs
+    -> Prod Unity rs
+    -> (forall s. Prod (BPRef s rs) rs -> BPRef s rs a)
+    -> Tuple rs
+    -> Tuple rs
+grad' ss us f = BP.gradBPOp' ss us (return $ f (BP.inpRefs' (prodLength ss)))
+
+grad
+    :: (Known Length rs, Every Num rs)
+    => (forall s. Prod (BPRef s rs) rs -> BPRef s rs a)
+    -> Tuple rs
+    -> Tuple rs
+grad f = BP.gradBPOp (return $ f BP.inpRefs)
+
