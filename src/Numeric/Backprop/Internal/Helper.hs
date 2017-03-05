2d1082d72e802aa8a14887f87e75dcf2f2181d00
Justin Le <justin@jle.im>
2017-03-05 00:40:07 -0800

    documented Helper.hs

--- src/Numeric/Backprop/Internal/Helper.hs
+++ src/Numeric/Backprop/Internal/Helper.hs
@@ -6,8 +6,22 @@
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+-- |
+-- Module      : Numeric.Backprop.Internal.Helper
+-- Copyright   : (c) Justin Le 2017
+-- License     : BSD3
+--
+-- Maintainer  : justin@jle.im
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- Provides general helper types like 'Summer' and 'Unity' that both
+-- "Numeric.Backprop.Op" and "Numeric.Backprop.Internal" use.
+
 module Numeric.Backprop.Internal.Helper (
+  -- * Summer
     Summer(..), summers, summers'
+  -- * Unity
   , Unity(..), unities, unities'
   ) where
 
@@ -17,9 +31,30 @@ import           Data.Type.Product
 import           Data.Type.Util
 import           Type.Class.Known
 
+-- | Instructions on how to "sum" a list of values of a given type.
+-- Basically used as an explicit witness for a 'Num' instance.
+--
+-- For most types, the only meaningful value of type @'Summer' a@ is
+-- @'Summer' 'sum'@.  However, using 'Summer' lets us use 'BP' with types
+-- that are /not/ instances of 'Num'.  Any type can be used, as long as you
+-- provide a way to "sum" it!
+--
+-- If 'Num a' is satisfied, one can create the canonical 'Summer' using
+-- @'known' :: 'Num' a => 'Summer' a@.
 newtype Summer a = Summer { runSummer :: [a] -> a }
+
+-- | A canonical "unity" (the multiplicative identity) for a given type.
+-- Basically used as an explicit witness for a 'Num' instance.
+--
+-- For most types, the only meaningful value of type @'Unity' a@ is
+-- @'Unity' 1'@.  However, using 'Unity' lets us use 'BP' with types
+-- that are /not/ instances of 'Num'.  Any type can be used, as long as you
+-- provide a way to get a multiplicative identity in it!
+--
+-- If 'Num a' is satisfied, one can create the canonical 'Unity' using
+-- @'known' :: 'Num' a => 'Unity' a@.
 newtype Unity  a = Unity  { getUnity  :: a        }
-    deriving (Functor, Show, Ord, Eq, Num, Fractional, Floating, Integral, Real, Enum, RealFloat, RealFrac)
+    deriving (Functor, Show)
 
 instance Num a => Known Summer a where
     type KnownC Summer a = Num a
@@ -29,22 +64,30 @@ instance Num a => Known Unity a where
     type KnownC Unity a = Num a
     known = Unity 1
 
+-- | If all the types in @as@ are instances of 'Num', generate a @'Prod'
+-- 'Summer' as@, or a tuple of 'Summer's for every type in @as@.
 summers
     :: (Every Num as, Known Length as)
     => Prod Summer as
 summers = summers' known
 
+-- | Like 'summers', but requiring an explicit witness for the number of
+-- types in the list @as@.
 summers'
     :: Every Num as
     => Length as
     -> Prod Summer as
 summers' l = withEvery' @Num l known
 
+-- | If all the types in @as@ are instances of 'Num', generate a @'Prod'
+-- 'Unity' as@, or a tuple of 'Unity's for every type in @as@.
 unities
     :: (Every Num as, Known Length as)
     => Prod Unity as
 unities = unities' known
 
+-- | Like 'unities', but requiring an explicit witness for the number of
+-- types in the list @as@.
 unities'
     :: Every Num as
     => Length as
